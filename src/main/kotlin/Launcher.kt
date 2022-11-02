05a11ac5cab953b76d6542b6fa8044dc265a6f4f
gyakkun <xiaowufeixia@gmail.com>
2022-11-02 18:17:40 +0800

    Extract group topic top post.

--- src/main/kotlin/Launcher.kt
+++ src/main/kotlin/Launcher.kt
@@ -14,12 +14,39 @@ class Launcher {
             var timing = System.currentTimeMillis()
 //            val converter: FlexmarkHtmlConverter = FlexmarkHtmlConverter.builder().build()
             val htmlContent: String =
-            FileUtil.getFileContent(File("E:\\SOURCE_ROOT\\bgm-archive-sh\\sample_html\\group_topic_sample.html"))!!
+                FileUtil.getFileContent(File("E:\\SOURCE_ROOT\\bgm-archive-sh\\sample_html\\group_topic_sample.html"))!!
 
-            val underTest: JXDocument = JXDocument.create(htmlContent)
-            val node: JXNode = underTest.selNOne(XPathHelper.XP_404_MSG)
+            val doc: JXDocument = JXDocument.create(htmlContent)
+            val groupNameAnchor: JXNode = doc.selNOne(XPathHelper.XP_GROUP_NAME_ANCHOR)
+            val topicTitle: JXNode = doc.selNOne(XPathHelper.XP_GROUP_TOPIC_TITLE_H1_TEXT)
+            val topPostDivSmallText = doc.selNOne(XPathHelper.XP_GROUP_TOPIC_TOP_POST_SMALL_TEXT)
+            val topPostUsernameAnchor = doc.selNOne(XPathHelper.XP_GROUP_TOPIC_TOP_POST_USERNAME_ANCHOR)
+            val topPostUidSpan = doc.selNOne(XPathHelper.XP_GROUP_TOPIC_TOP_POST_UID_SPAN)
+            val topPostUserNicknameAnchorText = doc.selNOne(XPathHelper.XP_GROUP_TOPIC_TOP_POST_USER_NICKNAME_ANCHOR_TEXT)
+            val topPostUserSignSpanText = doc.selNOne(XPathHelper.XP_GROUP_TOPIC_TOP_POST_USER_SIGN_SPAN_TEXT)
+            val topPostContentDiv = doc.selNOne(XPathHelper.XP_GROUP_TOPIC_TOP_POST_CONTENT_DIV)
 
-            System.err.println(node)
+
+
+            // group name: /group/{groupName}
+            System.err.println(groupNameAnchor.asElement().attr("href"))
+            // group display name: {groupDisplayName}
+            System.err.println(groupNameAnchor.asElement().text())
+            // title
+            System.err.println(topicTitle.asString())
+            // top post div small : #1 - {yyyy-MM-dd HH:mm}
+            System.err.println(topPostDivSmallText.asString())
+            // username: /user/{username}
+            System.err.println(topPostUsernameAnchor.asElement().attr("href"))
+            // user nickname: {nickname}
+            System.err.println(topPostUserNicknameAnchorText.asString())
+            // uid: background-image:url\\('//lain.bgm.tv/pic/user/l/\\d+/\\d+/\\d+/(\\d+)\\.jpg\\?r=\\d+'\\)
+            System.err.println(topPostUidSpan.asElement().attr("style"))
+            // user sign: ({sign})
+            System.err.println(topPostUserSignSpanText.asString())
+            // top post inner html: {}
+            System.err.println(topPostContentDiv.asElement().html())
+
 
 
 //            val outputFile = File("E:\\SOURCE_ROOT\\bgm-archive-sh\\sample_html\\group_topic_sample.md")
--- src/main/kotlin/moe/nyamori/bgm/util/XPathHelper.kt
+++ src/main/kotlin/moe/nyamori/bgm/util/XPathHelper.kt
@@ -4,11 +4,14 @@ object XPathHelper {
 
     val XP_404_MSG: String = "//div[1]/div[2]/div[1]/div/div/p[1]"
 
-    val XP_GROUP_NAME: String = "//div[2]/h1/span/a[1]"
-    val XP_GROUP_TOPIC_TITLE: String = "//div[2]/h1/text()"
+    val XP_GROUP_NAME_ANCHOR: String = "//div[2]/h1/span/a[1]"
+    val XP_GROUP_TOPIC_TITLE_H1_TEXT: String = "//div[2]/h1/text()"
     val XP_GROUP_TOPIC_TOP_POST_DIV: String = "//div[3]/div[1]"
-    val XP_GROUP_TOPIC_TOP_POST_SMALL_NUM_AND_DATE: String = "//div[3]/div[1]/div[1]/small/text()"
-    val XP_GROUP_TOPIC_TOP_POST_A_FOR_USERNAME: String = "//div[3]/div[1]/a"
-    val XP_GROUP_TOPIC_TOP_POST_A_FOR_USERID: String = "//div[3]/div[1]/a/span"
+    val XP_GROUP_TOPIC_TOP_POST_SMALL_TEXT: String = "//div[3]/div[1]/div[1]/small/text()"
+    val XP_GROUP_TOPIC_TOP_POST_USERNAME_ANCHOR: String = "//div[3]/div[1]/a"
+    val XP_GROUP_TOPIC_TOP_POST_UID_SPAN: String = "//div[3]/div[1]/a/span"
+    val XP_GROUP_TOPIC_TOP_POST_USER_NICKNAME_ANCHOR_TEXT: String = "//div[3]/div[1]/div[2]/strong/a/text()"
+    val XP_GROUP_TOPIC_TOP_POST_USER_SIGN_SPAN_TEXT: String = "//div[3]/div[1]/div[2]/span/text()"
+    val XP_GROUP_TOPIC_TOP_POST_CONTENT_DIV: String = "//div[3]/div[1]/div[2]/div"
 
 }
