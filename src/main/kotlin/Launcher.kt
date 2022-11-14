a20b308b9d2341786e691158974e305a6bd5048c
gyakkun <xiaowufeixia@gmail.com>
2022-11-15 01:20:30 +0800

    Stash the Launcher.

--- src/main/kotlin/Launcher.kt
+++ src/main/kotlin/Launcher.kt
@@ -1,21 +1,107 @@
+import com.google.gson.GsonBuilder
 import com.vladsch.flexmark.util.misc.FileUtil
-import moe.nyamori.bgm.util.XPathHelper
-import org.seimicrawler.xpath.JXDocument
-import org.seimicrawler.xpath.JXNode
+import moe.nyamori.bgm.config.Config
+import moe.nyamori.bgm.model.SpaceType
+import moe.nyamori.bgm.parser.TopicParser
+import org.slf4j.LoggerFactory
 import java.io.File
+import java.io.FileWriter
 import java.io.IOException
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.ConcurrentSkipListSet
+import java.util.concurrent.atomic.AtomicLong
+import kotlin.streams.asStream
 
 class Launcher {
-
     companion object {
+        val LOGGER = LoggerFactory.getLogger(Launcher.javaClass)
+        val gson = GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create()
+
         @Throws(IOException::class)
         @JvmStatic
         fun main(args: Array<String>) {
-            var timing = System.currentTimeMillis()
 //            val converter: FlexmarkHtmlConverter = FlexmarkHtmlConverter.builder().build()
-            val htmlContent: String =
-                FileUtil.getFileContent(File("E:\\SOURCE_ROOT\\bgm-archive-sh\\sample_html\\group_topic_sample_2.html"))!!
+            val archiveFolder = File(Config.BGM_ARCHIVE_GIT_REPO_DIR)
+//            val sampleFile1 = File("E:\\SOURCE_ROOT\\bgm-archive-sh\\sample_html\\group_topic_sample.html")
+//            val sampleFile2 = File("E:\\SOURCE_ROOT\\bgm-archive-sh\\sample_html\\group_topic_sample_2.html")
+//            val sampleFile2 = File("C:\\Users\\Steve\\source\\bgm-archive\\group\\36\\27\\362716.html")
+//            val sampleFile2 = File("C:\\Users\\Steve\\source\\bgm-archive-historical\\group\\00\\22\\2226.html")
+            val sampleFile2 = File("C:\\Users\\Steve\\source\\group\\374427.html")
+            val str = FileUtil.getFileContent(sampleFile2)!!
+            val (parseGroupTopic, result) = TopicParser.parseTopic(str, 374427,SpaceType.GROUP)
+            if (result) {
+                val toJson = gson.toJson(parseGroupTopic)
+                val jsonFile = File(
+                    sampleFile2.absolutePath.replace("bgm-archive", "bgm-archive-json")
+                        .replace(".html", ".json")
+                )
+                jsonFile.parentFile.mkdirs()
+                val fileWriter =
+                    FileWriter(
+                        jsonFile
+                    )
+                fileWriter.write(toJson)
+                fileWriter.flush()
+                fileWriter.close()
+                System.err.println(parseGroupTopic)
+            }
+//
+            if (true) return
 
+            val ts = ConcurrentSkipListSet<Pair<Long, File>>(Comparator.comparingLong { -it.first })
+            val ng = ConcurrentHashMap.newKeySet<File>()
+            val ngCounter = AtomicLong()
+            val totalCounter = AtomicLong()
+            val _404Counter = AtomicLong()
+            archiveFolder.walk().asStream().parallel().forEach allMatch@{ htmlFile ->
+                if (htmlFile.isDirectory) return@allMatch
+                if (htmlFile.extension != "html") return@allMatch
+                totalCounter.incrementAndGet()
+                var timing = System.currentTimeMillis()
+                val fileName = htmlFile.nameWithoutExtension
+                val fileStr = FileUtil.getFileContent(htmlFile)!!
+                val topicId = fileName.toInt()
+                val (topic, result) = TopicParser.parseTopic(fileStr, topicId,SpaceType.GROUP)
+                if (!result) {
+                    ng.add(htmlFile)
+                    ngCounter.incrementAndGet()
+//                    LOGGER.error("NG: ${htmlFile.absolutePath}")
+                    return@allMatch
+                } else {
+                    if (topic == null) {
+                        _404Counter.incrementAndGet()
+                        return@allMatch
+                    }
+                    timing = System.currentTimeMillis() - timing
+                    // LOGGER.info("$timing ms : ${htmlFile.absolutePath}")
+                    ts.add(Pair(timing, htmlFile))
+                    if (!topic!!.display!!) return@allMatch
+//                    val toJson = gson.toJson(topic)
+//                    val jsonFile = File(
+//                        htmlFile.absolutePath.replace("bgm-archive", "bgm-archive-json")
+//                            .replace(".html", ".json")
+//                    )
+//                    jsonFile.parentFile.mkdirs()
+//                    val fileWriter =
+//                        FileWriter(
+//                            jsonFile
+//                        )
+//                    fileWriter.write(toJson)
+//                    fileWriter.flush()
+//                    fileWriter.close()
+                    return@allMatch
+                }
+            }
+
+            LOGGER.info("NG COUNT: ${ngCounter.get()}")
+            LOGGER.info("404 COUNT: ${_404Counter.get()}")
+            LOGGER.info("TOT COUNT: ${totalCounter.get()}")
+            LOGGER.info("Top 10 time consumed: ${ts.toList().subList(0, 10)}")
+            LOGGER.info("NG: ${ng.map { i -> i.nameWithoutExtension }.sorted().toList()}")
+            /*
+            val htmlContent: String =
+                FileUtil.getFileContent(sampleFile2)!!
             val doc: JXDocument = JXDocument.create(htmlContent)
 
             val groupNameAnchor: JXNode = doc.selNOne(XPathHelper.XP_GROUP_NAME_ANCHOR)
@@ -121,8 +207,8 @@ class Launcher {
 //            bfw.flush()
 //            bfw.close()
 //            fw.close()
-            timing = System.currentTimeMillis() - timing
-            System.err.println("TOTAL: " + timing + "ms.")
+             */
+
         }
     }
 }
