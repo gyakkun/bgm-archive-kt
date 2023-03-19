1c15b8db639490f6266acfef2e3eb71caf41eec4
gyakkun <xiaowufeixia@gmail.com>
2023-03-19 21:44:24 +0800

    Fix.

--- src/main/kotlin/moe/nyamori/bgm/HttpServer.kt
+++ src/main/kotlin/moe/nyamori/bgm/HttpServer.kt
@@ -70,84 +70,95 @@ class HttpServer {
 
         class FileHistory(private val spaceType: SpaceType) : Handler {
             override fun handle(ctx: Context) {
-                if (!lock.tryLock(30, TimeUnit.SECONDS)) {
-                    ctx.status(HttpStatus.REQUEST_TIMEOUT)
-                    ctx.html("The server is busy. Please wait and refresh later.")
-                    return
+                try {
+                    if (!lock.tryLock(30, TimeUnit.SECONDS)) {
+                        ctx.status(HttpStatus.REQUEST_TIMEOUT)
+                        ctx.html("The server is busy. Please wait and refresh later.")
+                        return
+                    }
+                    val topicId = ctx.pathParam("topicId").toInt()
+                    val timestampList = FileHistoryLookup.getJsonTimestampList(
+                        spaceType.name.lowercase() + "/" + FilePathHelper.numberToPath(topicId) + ".json"
+                    )
+                    ctx.header(CACHE_CONTROL, "max-age=3600")
+                    ctx.json(timestampList)
+                } catch (ex: Exception) {
+                    log.error("Ex: ", ex)
+                    throw ex
+                } finally {
+                    if (lock.isHeldByCurrentThread) lock.unlock()
                 }
-                val topicId = ctx.pathParam("topicId").toInt()
-                val timestampList = FileHistoryLookup.getJsonTimestampList(
-                    spaceType.name.lowercase() + "/" + FilePathHelper.numberToPath(topicId) + ".json"
-                )
-                ctx.header(CACHE_CONTROL, "max-age=3600")
-                ctx.json(timestampList)
-                if (lock.isHeldByCurrentThread) lock.unlock()
             }
         }
 
         class FileOnCommit(private val spaceType: SpaceType, private val isRaw: Boolean = false) : Handler {
             override fun handle(ctx: Context) {
-                if (!lock.tryLock(30, TimeUnit.SECONDS)) {
-                    ctx.status(HttpStatus.REQUEST_TIMEOUT)
-                    ctx.html("The server is busy. Please wait and refresh later.")
-                    return
-                }
-                val topicId = ctx.pathParam("topicId").toInt()
-                val timestampPathParam = ctx.pathParam("timestamp")
-                val timestamp =
-                    if (timestampPathParam == "latest") Long.MAX_VALUE
-                    else if (timestampPathParam.toLongOrNull() != null) timestampPathParam.toLong()
-                    else -1L
-                val relativePath =
-                    spaceType.name.lowercase() + "/" + FilePathHelper.numberToPath(topicId) + if (isRaw) ".html" else ".json"
-                val timestampList = if (isRaw) {
-                    FileHistoryLookup.getArchiveTimestampList(relativePath)
-                } else {
-                    FileHistoryLookup.getJsonTimestampList(relativePath)
-                }
-                val ts = TreeSet<Long>().apply {
-                    addAll(timestampList)
-                }
-                if (!ts.contains(timestamp)) {
-                    var ceilingTimestamp = ts.floor(timestamp)
-                    if (ceilingTimestamp == null) {
-                        ceilingTimestamp = ts.first()
+                try {
+                    if (!lock.tryLock(30, TimeUnit.SECONDS)) {
+                        ctx.status(HttpStatus.REQUEST_TIMEOUT)
+                        ctx.html("The server is busy. Please wait and refresh later.")
+                        return
                     }
-                    ctx.redirect(
-                        if (isRaw) {
-                            ctx.path().replace(Regex("/${timestampPathParam}/html"), "/${ceilingTimestamp}/html")
-                        } else {
-                            ctx.path().replace(Regex("/${timestampPathParam}/*\$"), "/${ceilingTimestamp}")
+                    val topicId = ctx.pathParam("topicId").toInt()
+                    val timestampPathParam = ctx.pathParam("timestamp")
+                    val timestamp =
+                        if (timestampPathParam == "latest") Long.MAX_VALUE
+                        else if (timestampPathParam.toLongOrNull() != null) timestampPathParam.toLong()
+                        else -1L
+                    val relativePath =
+                        spaceType.name.lowercase() + "/" + FilePathHelper.numberToPath(topicId) + if (isRaw) ".html" else ".json"
+                    val timestampList = if (isRaw) {
+                        FileHistoryLookup.getArchiveTimestampList(relativePath)
+                    } else {
+                        FileHistoryLookup.getJsonTimestampList(relativePath)
+                    }
+                    val ts = TreeSet<Long>().apply {
+                        addAll(timestampList)
+                    }
+                    if (!ts.contains(timestamp)) {
+                        var ceilingTimestamp = ts.floor(timestamp)
+                        if (ceilingTimestamp == null) {
+                            ceilingTimestamp = ts.first()
                         }
-                    )
-                    return
-                }
+                        ctx.redirect(
+                            if (isRaw) {
+                                ctx.path().replace(Regex("/${timestampPathParam}/html"), "/${ceilingTimestamp}/html")
+                            } else {
+                                ctx.path().replace(Regex("/${timestampPathParam}/*\$"), "/${ceilingTimestamp}")
+                            }
+                        )
+                        return
+                    }
 
-                ctx.header(CACHE_CONTROL, "max-age=86400")
-                if (isRaw) {
-                    var html = GitHelper.getFileContentInACommit(
-                        GitHelper.archiveRepoSingleton,
-                        FileHistoryLookup.getArchiveCommitAtTimestamp(relativePath, timestamp),
-                        relativePath
-                    )
-                    html = html.replace("chii.in", "bgm.tv")
-                    html = html.replace("bangumi.tv", "bgm.tv")
-                    html = html.replace("//lain.bgm.tv", "https://lain.bgm.tv")
-                    html = html.replace("src=\"/", "src=\"https://bgm.tv/")
-                    html = html.replace("href=\"/", "href=\"https://bgm.tv/")
-                    ctx.html(html)
-                } else {
-                    ctx.json(
-                        GitHelper.getFileContentInACommit(
-                            GitHelper.jsonRepoSingleton,
-                            FileHistoryLookup.getJsonCommitAtTimestamp(relativePath, timestamp),
+                    ctx.header(CACHE_CONTROL, "max-age=86400")
+                    if (isRaw) {
+                        var html = GitHelper.getFileContentInACommit(
+                            GitHelper.archiveRepoSingleton,
+                            FileHistoryLookup.getArchiveCommitAtTimestamp(relativePath, timestamp),
                             relativePath
                         )
-                    )
+                        html = html.replace("chii.in", "bgm.tv")
+                        html = html.replace("bangumi.tv", "bgm.tv")
+                        html = html.replace("//lain.bgm.tv", "https://lain.bgm.tv")
+                        html = html.replace("src=\"/", "src=\"https://bgm.tv/")
+                        html = html.replace("href=\"/", "href=\"https://bgm.tv/")
+                        ctx.html(html)
+                    } else {
+                        ctx.json(
+                            GitHelper.getFileContentInACommit(
+                                GitHelper.jsonRepoSingleton,
+                                FileHistoryLookup.getJsonCommitAtTimestamp(relativePath, timestamp),
+                                relativePath
+                            )
+                        )
+                    }
+                } catch (ex: Exception) {
+                    log.error("Ex: ",ex)
+                    throw ex
+                } finally {
+                    if (lock.isHeldByCurrentThread) lock.unlock()
                 }
-                if (lock.isHeldByCurrentThread) lock.unlock()
             }
-
         }
 
         object LinkHandler : Handler {
