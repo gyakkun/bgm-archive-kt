b2d7909343f6119a75af8337af89f8b9edba5b35
gyakkun <xiaowufeixia@gmail.com>
2023-04-20 11:00:27 +0800

    Add config for historical static repos.

--- src/main/kotlin/moe/nyamori/bgm/config/Config.kt
+++ src/main/kotlin/moe/nyamori/bgm/config/Config.kt
@@ -4,6 +4,10 @@ import java.io.File
 
 object Config {
 
+    // For bare repo
+    // Please run: git config remote.origin.fetch 'refs/heads/*:refs/heads/*'
+    // to make fetch same as pull
+
     var BGM_ARCHIVE_GIT_REPO_DIR: String = System.getenv().getOrDefault(
         "E_BGM_ARCHIVE_GIT_REPO",
         File(System.getProperty("user.home")).resolve("source/bgm-archive").absolutePath
@@ -12,6 +16,15 @@ object Config {
     var BGM_ARCHIVE_JSON_GIT_REPO_DIR: String = System.getenv().getOrDefault(
         "E_BGM_ARCHIVE_JSON_GIT_REPO",
         File(System.getProperty("user.home")).resolve("source/bgm-archive-json").absolutePath
+    )
+
+    // Comma seperated
+    var BGM_ARCHIVE_GIT_STATIC_REPO_DIR_LIST: String = System.getenv().getOrDefault(
+        "E_BGM_ARCHIVE_GIT_STATIC_REPO_DIR_LIST", ""
+    )
+
+    var BGM_ARCHIVE_JSON_GIT_STATIC_REPO_DIR_LIST: String = System.getenv().getOrDefault(
+        "E_BGM_ARCHIVE_JSON_GIT_STATIC_REPO_DIR_LIST", ""
     )
 
     var BGM_ARCHIVE_PREV_PROCESSED_COMMIT_REV_ID_FILE_NAME: String = System.getenv().getOrDefault(
