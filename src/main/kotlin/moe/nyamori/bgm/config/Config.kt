90af7de1656539f5e27fef424cfdc5ebc85b6833
gyakkun <xiaowufeixia@gmail.com>
2023-04-25 21:23:22 +0800

    Add config for sqlite file. Refactor.

--- src/main/kotlin/moe/nyamori/bgm/config/Config.kt
+++ src/main/kotlin/moe/nyamori/bgm/config/Config.kt
@@ -8,39 +8,44 @@ object Config {
     // Please run: git config remote.origin.fetch 'refs/heads/*:refs/heads/*'
     // to make fetch same as pull
 
-    var BGM_ARCHIVE_GIT_REPO_DIR: String = System.getenv().getOrDefault(
+    val BGM_ARCHIVE_GIT_REPO_DIR: String = System.getenv().getOrDefault(
         "E_BGM_ARCHIVE_GIT_REPO",
         File(System.getProperty("user.home")).resolve("source/bgm-archive").absolutePath
     )
 
-    var BGM_ARCHIVE_JSON_GIT_REPO_DIR: String = System.getenv().getOrDefault(
+    val BGM_ARCHIVE_JSON_GIT_REPO_DIR: String = System.getenv().getOrDefault(
         "E_BGM_ARCHIVE_JSON_GIT_REPO",
         File(System.getProperty("user.home")).resolve("source/bgm-archive-json").absolutePath
     )
 
     // Comma seperated
-    var BGM_ARCHIVE_GIT_STATIC_REPO_DIR_LIST: String = System.getenv().getOrDefault(
+    val BGM_ARCHIVE_GIT_STATIC_REPO_DIR_LIST: String = System.getenv().getOrDefault(
         "E_BGM_ARCHIVE_GIT_STATIC_REPO_DIR_LIST", ""
     )
 
-    var BGM_ARCHIVE_JSON_GIT_STATIC_REPO_DIR_LIST: String = System.getenv().getOrDefault(
+    val BGM_ARCHIVE_JSON_GIT_STATIC_REPO_DIR_LIST: String = System.getenv().getOrDefault(
         "E_BGM_ARCHIVE_JSON_GIT_STATIC_REPO_DIR_LIST", ""
     )
 
-    var BGM_ARCHIVE_PREV_PROCESSED_COMMIT_REV_ID_FILE_NAME: String = System.getenv().getOrDefault(
+    val BGM_ARCHIVE_PREV_PROCESSED_COMMIT_REV_ID_FILE_NAME: String = System.getenv().getOrDefault(
         "E_BGM_ARCHIVE_PREV_PROCESSED_COMMIT_REV_ID_FILE_NAME",
         "last_processed_commit_rev_id"
     )
 
-    var BGM_ARCHIVE_PREFER_JGIT: Boolean =
+    val BGM_ARCHIVE_PREFER_JGIT: Boolean =
         System.getenv().getOrDefault("E_BGM_ARCHIVE_PREFER_JGIT", "false").toBoolean()
 
-    var BGM_ARCHIVE_DISABLE_HOOK: Boolean =
+    val BGM_ARCHIVE_DISABLE_HOOK: Boolean =
         System.getenv().getOrDefault("E_BGM_ARCHIVE_DISABLE_HOOK", "false").toBoolean()
 
-    var BGM_ARCHIVE_ADDRESS: String =
+    val BGM_ARCHIVE_ADDRESS: String =
         System.getenv().getOrDefault("E_BGM_ARCHIVE_ADDRESS", "localhost")
 
-    var BGM_ARCHIVE_PORT: Int =
+    val BGM_ARCHIVE_PORT: Int =
         System.getenv().getOrDefault("E_BGM_ARCHIVE_PORT", "5926").toInt()
+
+    val BGM_ARCHIVE_SQLITE_FILE: String = System.getenv().getOrDefault(
+        "E_BGM_ARCHIVE_SQLITE_FILE",
+        File(System.getProperty("user.home")).resolve("source/bgm-archive-db/bgm-archive.sqlite").absolutePath
+    )
 }
