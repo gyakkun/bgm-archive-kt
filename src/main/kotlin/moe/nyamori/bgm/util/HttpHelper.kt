35faee94b12a98459d882076814e745f631df236
gyakkun <xiaowufeixia@gmail.com>
2023-05-07 17:16:04 +0800

    Add semaphore for db query.

--- src/main/kotlin/moe/nyamori/bgm/util/HttpHelper.kt
+++ src/main/kotlin/moe/nyamori/bgm/util/HttpHelper.kt
@@ -2,11 +2,12 @@ package moe.nyamori.bgm.util
 
 import io.javalin.http.Context
 import moe.nyamori.bgm.model.SpaceType
-import java.lang.IllegalArgumentException
+import java.util.concurrent.Semaphore
 import java.util.concurrent.locks.ReentrantLock
 
 object HttpHelper {
     val GIT_RELATED_LOCK = ReentrantLock()
+    val DB_RELATED_SEMAPHORE = Semaphore(5)
     fun checkAndExtractSpaceTypeInContext(ctx: Context): SpaceType {
         val spaceTypeFromPath = ctx.pathParam("spaceType")
         if (SpaceType.values().none {
