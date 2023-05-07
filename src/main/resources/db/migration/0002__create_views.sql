f49a772b20505df37b814a714e398ed688d72a28
gyakkun <xiaowufeixia@gmail.com>
2023-05-07 15:07:33 +0800

    Sorting.

--- src/main/resources/db/migration/0002__create_views.sql
+++ src/main/resources/db/migration/0002__create_views.sql
@@ -19,8 +19,8 @@ create view if not exists ba_v_topic_username_rank_by_last_reply_and_dateline as
 select bt.*,
        bp.dateline as                                                      last_update_time,
        bu.username as                                                      username,
-       rank() over (partition by bt.type,bt.uid order by bp.dateline desc) rank_last_reply,
-       rank() over (partition by bt.type,bt.uid order by bp.dateline desc) rank_dateline
+       rank() over (partition by bt.type,bt.uid order by bp.dateline, bp.id desc) rank_last_reply,
+       rank() over (partition by bt.type,bt.uid order by bp.dateline, bp.id desc) rank_dateline
 from ba_topic bt
          inner join ba_post bp on bt.last_post_pid = bp.id and bt.type = bp.type
          inner join ba_user bu on bu.id = bt.uid;
