b968093804383d30a3a5e0c9f50e4830dc25d672
gyakkun <xiaowufeixia@gmail.com>
2023-05-18 19:28:49 +0800

    Update state helper.

--- src/main/kotlin/moe/nyamori/bgm/util/PostToStateHelper.kt
+++ src/main/kotlin/moe/nyamori/bgm/util/PostToStateHelper.kt
@@ -4,9 +4,10 @@ import moe.nyamori.bgm.model.Post
 
 object PostToStateHelper {
     fun fromPostHtmlToState(postHtml: String): Long {
-        if (!postHtml.startsWith("<span class=\"tip\">")) return Post.STATE_NORMAL
+        if (!postHtml.startsWith("<span class=\"tip")) return Post.STATE_NORMAL
         if (postHtml.contains("内容已被用户删除")) return Post.STATE_DELETED
-        if (postHtml.contains("内容因违反")) return Post.STATE_ADMIN_DELETED
+        if (postHtml.contains("删除了回复")) return Post.STATE_DELETED
+        if (postHtml.contains("因违反")) return Post.STATE_ADMIN_DELETED
         return Post.STATE_NORMAL
     }
 }
--- src/test/kotlin/moe/nyamori/bgm/GeneralTest.kt
+++ src/test/kotlin/moe/nyamori/bgm/GeneralTest.kt
@@ -1,7 +1,9 @@
 package moe.nyamori.bgm
 
+import com.vladsch.flexmark.util.misc.FileUtil
 import moe.nyamori.bgm.git.GitHelper
 import moe.nyamori.bgm.model.*
+import java.io.File
 import java.io.IOException
 import java.util.*
 
@@ -10,9 +12,21 @@ class GeneralTest {
         @Throws(IOException::class)
         @JvmStatic
         fun main(args: Array<String>) {
-            // val commit = GitHelper.jsonRepoSingleton.getRevCommitById("sjfklasjfklsajfldkjsalk")
-            val commit = GitHelper.getPrevPersistedJsonCommitRef()
-            System.err.println(commit)
+            System.err.println("hello world")
+            //val oldMask = File("E:\\[ToBak]\\Desktop_Win10\\old_sub_hid_mask.txt")
+            //val newMask = File("E:\\[ToBak]\\Desktop_Win10\\new_sub_hid_mask.txt")
+            //val fromFileToBs = fun(file: File): BitSet {
+            //    val longArr = FileUtil.getFileContent(file)!!.lines().mapNotNull { it.toLongOrNull() }.toLongArray()
+            //    return BitSet.valueOf(longArr)
+            //}
+            //val oldBs = fromFileToBs(oldMask)
+            //val newBs = fromFileToBs(newMask)
+            //val newClone = newBs.clone() as BitSet
+//
+            //newBs.xor(oldBs)
+            //newBs.and(newClone)
+//
+            //System.err.println("Not masked in old but masked in new ${newBs}")
         }
     }
 }
