5d362e8f46c5c7a61638e32d870e919ed2f3b478
gyakkun <xiaowufeixia@gmail.com>
2023-05-18 19:33:54 +0800

    Add test cases.

---
+++ src/test/kotlin/moe/nyamori/bgm/util/PostToStateHelperTest.kt
@@ -0,0 +1,21 @@
+package moe.nyamori.bgm.util
+
+import moe.nyamori.bgm.model.Post
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class PostToStateHelperTest {
+    @Test
+    fun r414DeletedPost() {
+        val post = "<span class=\"tip_collapsed\">删除了回复</span>"
+        val state = PostToStateHelper.fromPostHtmlToState(post)
+        assertEquals(state, Post.STATE_DELETED)
+    }
+
+    @Test
+    fun r414AdminDeletedPost() {
+        val post = "<span class=\"tip_collapsed\">因违反「<a class=\"l\" href=\"/about/guideline\">社区指导原则</a>」已被删除</span>"
+        val state = PostToStateHelper.fromPostHtmlToState(post)
+        assertEquals(state, Post.STATE_ADMIN_DELETED)
+    }
+}
