d4af65af4826cdb99113ef0f4720656645632c87
gyakkun <xiaowufeixia@gmail.com>
2023-07-04 20:23:50 +0800

    Add other repo status.

--- src/main/kotlin/moe/nyamori/bgm/HttpServer.kt
+++ src/main/kotlin/moe/nyamori/bgm/HttpServer.kt
@@ -56,18 +56,22 @@ class HttpServer {
                                 }.associate { Pair(it[0].trim(), it[1].trim()) }
                             },
                             "otherRepoStatus" to run {
-                                Config.BGM_ARCHIVE_GIT_STATIC_REPO_DIR_LIST.split(",")
-                                    .filter { it.isNotBlank() }
-                                    .map { it.trim() }
-                                    .map { File(it) }.associate {
-                                        it.path.split("/").last() to run {
-                                            val gitProcess = Runtime.getRuntime()
-                                                .exec("git count-objects -vH", null, it)
-                                            gitProcess.blockAndPrintProcessResults().map {
-                                                it.split(":")
-                                            }.associate { Pair(it[0].trim(), it[1].trim()) }
-                                        }
+                                mutableListOf<String>().apply {
+                                    addAll(Config.BGM_ARCHIVE_GIT_STATIC_REPO_DIR_LIST.split(",")
+                                        .filter { it.isNotBlank() }
+                                        .map { it.trim() })
+                                    addAll(Config.BGM_ARCHIVE_JSON_GIT_STATIC_REPO_DIR_LIST.split(",")
+                                        .filter { it.isNotBlank() }
+                                        .map { it.trim() })
+                                }.map { File(it) }.associate {
+                                    it.path.split("/").last() to run {
+                                        val gitProcess = Runtime.getRuntime()
+                                            .exec("git count-objects -vH", null, it)
+                                        gitProcess.blockAndPrintProcessResults().map {
+                                            it.split(":")
+                                        }.associate { Pair(it[0].trim(), it[1].trim()) }
                                     }
+                                }
                             }
                         )
                     )
