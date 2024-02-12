50b226ef8d0fa1b4db077b13954d9cacab685e90
gyakkun <xiaowufeixia@gmail.com>
2024-02-12 17:04:10 +0800

    Thread.

--- src/main/kotlin/moe/nyamori/bgm/util/ExternalProcessHelper.kt
+++ src/main/kotlin/moe/nyamori/bgm/util/ExternalProcessHelper.kt
@@ -1,10 +1,10 @@
 package moe.nyamori.bgm.util
 
+import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.concurrent.CopyOnWriteArrayList
 import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
-import kotlin.text.Charsets.UTF_8
+import kotlin.concurrent.thread
 
 fun Process.blockAndPrintProcessResults(
     toLines: Boolean = true,
@@ -13,20 +13,25 @@ fun Process.blockAndPrintProcessResults(
     val result = CopyOnWriteArrayList<String?>()
     val latch = CountDownLatch(2)
     // Here actually block the process
-    val stderr = this.errorStream
-    val stdout = this.inputStream
-    listOf(stderr, stdout).map { out ->
-        out.use { outUsing ->
-            InputStreamReader(outUsing, UTF_8).use { isr ->
-                if (!toLines) result.add(isr.readText())
-                else result.addAll(isr.readText().lines())
-
-                if(printAtStdErr) System.err.println(result)
+    listOf(this.errorStream, this.inputStream).forEach { out ->
+        thread {
+            out.use { outUsing ->
+                InputStreamReader(outUsing).use { isr ->
+                    if (!toLines) result.add(isr.readText())
+                    else
+                        BufferedReader(isr).use { reader ->
+                            var line: String?
+                            while (reader.readLine().also { line = it } != null) {
+                                if (printAtStdErr) System.err.println(line)
+                                result.add(line)
+                            }
+                        }
+                }
             }
+            latch.countDown()
         }
-        latch.countDown()
     }
-    latch.await(2,TimeUnit.MINUTES)
     this.waitFor()
+    latch.await()
     return result.filterNotNull()
 }
