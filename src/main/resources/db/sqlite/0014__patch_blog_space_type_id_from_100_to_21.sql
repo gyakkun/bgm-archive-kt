a584a6f386570610273bfa6f160b15ddfaafb3f8
gyakkun <xiaowufeixia@gmail.com>
2025-09-06 17:37:08 +0800

    Add data patch.

---
+++ src/main/resources/db/sqlite/0014__patch_blog_space_type_id_from_100_to_21.sql
@@ -0,0 +1,5 @@
+update ba_likes set type = 21 where type = 100;
+update ba_likes_rev set type = 21 where type = 100;
+update ba_post set type = 21 where type = 100;
+update ba_space_naming_mapping set type = 21 where type = 100;
+update ba_topic set type = 21 where type = 100;
