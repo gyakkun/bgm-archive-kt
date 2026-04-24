21bdffa612cd356714da2ecf71f32051a67311fc
gyakkun <xiaowufeixia@gmail.com>
2026-04-25 04:12:02 +0800

    Address warnings.

--- src/test/kotlin/moe/nyamori/bgm/git/SpotCheckerCucumberTest.kt
+++ src/test/kotlin/moe/nyamori/bgm/git/SpotCheckerCucumberTest.kt
@@ -2,12 +2,12 @@ package moe.nyamori.bgm.git
 
 import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
-import org.junit.platform.suite.api.SelectClasspathResource
+import org.junit.platform.suite.api.SelectPackages
 import org.junit.platform.suite.api.Suite
 import io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
+@SelectPackages("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "moe.nyamori.bgm.git")
 class SpotCheckerCucumberTest
