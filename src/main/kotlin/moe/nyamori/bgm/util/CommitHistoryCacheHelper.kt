604f9ee099f99f4d1edb136416e908ab0f3fd765
gyakkun <xiaowufeixia@gmail.com>
2026-07-30 00:23:06 +0800

    Revert "Logging. Revert it."
    This reverts commit 14cd4c1a0e3e4be25ee705e8ac5d94b04cedfe1a.

--- src/main/kotlin/moe/nyamori/bgm/util/CommitHistoryCacheHelper.kt
+++ src/main/kotlin/moe/nyamori/bgm/util/CommitHistoryCacheHelper.kt
@@ -13,14 +13,7 @@ import kotlin.random.Random
 
 object CommitHistoryCacheHelper {
     private val LOGGER = LoggerFactory.getLogger(CommitHistoryCacheHelper::class.java)
-    private fun logTime(step:String, startTime:Long, lastStepTime:Long):Long {
-        val cur = System.currentTimeMillis()
-        LOGGER.info("${step} takes ${ cur-lastStepTime }ms, elapsed: ${startTime - cur}ms")
-        return cur
-    }
     fun Repository.buildCache() {
-        val startTime = System.currentTimeMillis()
-        var lastStepTime = startTime
         check(HttpHelper.DB_WRITE_LOCK.isHeldByCurrentThread)
         val jgitToggle = Config.isUseJgitInCacheBuild
         val repo = this
@@ -28,19 +21,14 @@ object CommitHistoryCacheHelper {
         var isFreshCacheBuild = false
         val prevCachedSha1 =
             Dao.bgmDao.getPrevCachedCommitId(this) ?: run { isFreshCacheBuild = true; this.getFirstCommitIdStr(useJgit = jgitToggle) }
-        lastStepTime = logTime("getPrevCachedSha1", startTime, lastStepTime)
         val latestSha1 = this.getLatestCommit(useJgit = jgitToggle).sha1
-        lastStepTime = logTime("getLatestSha1", startTime, lastStepTime)
         val firstCommitSha1Getter = { this.getFirstCommitIdStr(useJgit = jgitToggle) }
-        lastStepTime = logTime("getFirstCommitSha1", startTime, lastStepTime)
 
         var prevCachedMsg = ""
         var latestMsg = ""
         runCatching {
             prevCachedMsg = this.getGivenCommitByIdStrOrFirstCommit(prevCachedSha1, useJgit = jgitToggle).fullMessage.trim().substringBefore("\n")
-            lastStepTime = logTime("getPrevCachedMsg", startTime, lastStepTime)
             latestMsg = this.getGivenCommitByIdStrOrFirstCommit(latestSha1, useJgit = jgitToggle).fullMessage.trim().substringBefore("\n")
-            lastStepTime = logTime("getLatestMsg", startTime, lastStepTime)
         }
 
         var counter = 0
@@ -56,22 +44,18 @@ object CommitHistoryCacheHelper {
                     Dao.bgmDao.updatePrevCachedCommitId(repo, curCommitId)
                     return@processHistory
                 }
-                lastStepTime = logTime("updatePrevCachedCommitId", startTime, lastStepTime)
-
+                
                 val shouldLog = Random.nextFloat() < 0.001F || Config.logCacheDetail
                 val changedFilePaths = files.filter { it.endsWith("html", ignoreCase = true) || it.endsWith("json", ignoreCase = true) }
                 if (shouldLog) LOGGER.info("[CACHE] $repo - cur commit: $curCommitId , msg - $curCommitFullMsg")
-                lastStepTime = logTime("changedFilePaths", startTime, lastStepTime)
 
                 // 1. Update changed files
                 val resIntArray = Dao.bgmDao.batchUpsertFileRelativePathForCache(changedFilePaths)
                 if (shouldLog) LOGGER.info("[CACHE] File ids insert res int array: {}", resIntArray)
-                lastStepTime = logTime("batchUpsertFileRelativePathForCache", startTime, lastStepTime)
 
                 // 2. Insert this commit to repo commit table
                 val resInt = Dao.bgmDao.insertRepoCommitForCache(repo.repoIdFromDto().toLong(), curCommitId)
                 if (shouldLog) LOGGER.info("[CACHE] commit insert res int: $resInt")
-                lastStepTime = logTime("insertRepoCommitForCache", startTime, lastStepTime)
 
                 // 3. Insert to file-commit
                 val cacheFileCommitInsertRes = Dao.bgmDao.batchUpsertFileCommitForCache(
@@ -80,12 +64,10 @@ object CommitHistoryCacheHelper {
                     curCommitId
                 )
                 if (shouldLog) LOGGER.info("[CACHE] Insert result: $cacheFileCommitInsertRes")
-                lastStepTime = logTime("batchUpsertFileCommitForCache", startTime, lastStepTime)
 
                 // 4. Update meta data
                 val updateMetaRes = Dao.bgmDao.updatePrevCachedCommitId(repo, curCommitId)
                 if (shouldLog) LOGGER.info("[CACHE] Update meta res: $updateMetaRes")
-                lastStepTime = logTime("updatePrevCachedCommitId", startTime, lastStepTime)
 
                 if (shouldLog)
                     LOGGER.info("[CACHE] $repo build cache for commit successfully - [$curCommitId - $curCommitFullMsg]")
@@ -112,7 +94,6 @@ object CommitHistoryCacheHelper {
                 """;
             LOGGER.error(sentence)
         }
-        lastStepTime = logTime("finished", startTime, lastStepTime)
     }
 
 }
